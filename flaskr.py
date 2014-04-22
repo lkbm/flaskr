d9830d5464800060c3f1a1c37a5ec8971020775a
LKBM <luca.masters@gmail.com>
2014-04-21 23:49:19 -0500

    Some database init code, which doesn't work even though it's straight from the tutorial, and some cleanup.

--- flaskr.py
+++ flaskr.py
@@ -1,3 +1,7 @@
+"""
+	Based on Flaskr by Armin Noacher. BSD license.
+"""
+
 # all the imports
 import os
 import sqlite3
@@ -5,6 +9,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 
 # create our little application :)
 app = Flask(__name__)
+
 app.config.from_object(__name__)
 
 # Load default config and override config from an environment variable
@@ -13,7 +18,7 @@ app.config.update(dict(
 	DEBUG=True,
 	SECRET_KEY='dev key',
 	USERNAME='admin',
-	PASSWORD='Reymundo$'
+    PASSWORD='secret'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
@@ -22,6 +27,13 @@ def connect_db():
 	rv = sqlite3.connect(app.config['DATABASE'])
 	rv.row_factory = sqlite3.Row
 	return rv
+
+def init_db():
+	with app.app_context():
+		db = get_db()
+		with app.open_resource('schema.sql', mode='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
 
 if __name__ == '__main__':
 	app.run()
