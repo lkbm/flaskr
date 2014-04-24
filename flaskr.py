c95a026d4bd622feb3eecdf658d43f6ef6c92641
LKBM <luca.masters@gmail.com>
2014-04-23 22:21:48 -0500

    Fixing template screwups. Still (maybe) broken: /logout

--- flaskr.py
+++ flaskr.py
@@ -7,9 +7,8 @@ import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
-# create our little application :)
+# Create the application
 app = Flask(__name__)
-
 app.config.from_object(__name__)
 
 # Load default config and override config from an environment variable
@@ -35,9 +34,6 @@ def init_db():
 			db.cursor().executescript(f.read())
 		db.commit()
 
-if __name__ == '__main__':
-	app.run()
-
 def get_db():
 	"""Opens a new database connection if there is none yet for the current application context."""
 	if not hasattr(g, 'sqlite_db'):
@@ -52,4 +48,45 @@ def close_db(error):
 	if hasattr(g, 'sqlite_db'):
 		g.sqlite_db.close()
 
+@app.route('/')
+def show_entries():
+	db = get_db()
+	cur = db.execute('select title, text from entries order by id desc')
+	entries = cur.fetchall()
+	return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+	if not session.get('logged_in'):
+		abort(401)
+	db = get_db()
+	db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
+	db.commit()
+	flash('New entry was successfully posted')
+	return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	#session['logged_in'] = False	# Maybe this will work. (Nope. Logging out still not working. Could be Polipo or Privoxy's fault.))
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
+
+if __name__ == '__main__':
+	init_db()
+	app.run()
 
